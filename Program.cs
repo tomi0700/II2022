6fc60e385bc57130ff5a84908a5b5efe7b0b38c2
Tomescu Razvan <92920572+tomi0700@users.noreply.github.com>
2022-03-08 11:52:59 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Ex1
+{
+    class Fibonacci
+    {
+        static void Main(string[] args)
+        {
+            int n, f0 = 0, f1 = 1, fibo = 0;
+            Console.WriteLine("Introduceti valoarea lui n");
+            n = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Secventa Fibonnaci este : ");
+                Console.Write("{0} {1} ", f0, f1);
+                for (int i = 3; i <= n; i++)
+                {
+                    fibo = f0 + f1;
+                    Console.Write("{0} ", fibo);
+                    f0 = f1;
+                    f1 = fibo;
+
+                }
+
+            }
+        }
+
+
+    }
+
